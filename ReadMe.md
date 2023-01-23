fbd3dc39e3ebf68cd167cddf63a6f665620831f1
laura-williams-1 <laurawilliams@pursuit.org>
2023-01-22 22:45:38 -0500

    done

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+Eat, Sleep, Cocktail, Repeat is a website for invdivduals that love a good drink.
+
+This website will alloq users to:
+1- Search for a cocktail by name
+2- Provide an image and insurctions on how to create the cocktail
+from search results
--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-const url = "www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+const button = document.getElementById("lucky");
+button.addEventListener("click", async () =>
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let drinkName = document.createElement("h3");
+      let drinkImage = document.createElement("img");
+      drinkName.innerHTML = data.drinks[0].strDrink;
+      drinkImage.src = data.drinks[0].strDrinkThumb;
+      let drinkInsuctions = document.createElement("p");
+      let drinkIng = document.createElement("p");
+      let drinkMes = document.createElement("p");
+
+      // let currentDrink = data.dinks[0];
+
+      // for (let i of currentDrink) {
+      // }
+
+      let randomSection = document.getElementById("random-drink-result");
+
+      randomSection.append(drinkName, drinkImage);
+      randomSection.prepend(button);
+    })
+);
+
+// function luckyCocktail() {
+//   fetch(url)
+//     .then(function (response) {
+//       response.json().then(function (data) {
+//         console.log(data);
+//         displayLuckDrink(data);
+//       });
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     });
+// }
+// luckyCocktail();
+
+// function displayLuckDrink(drink) {
+//   console.log(drink.drinks[0].strDrink);
+
+//   let randomSearch = document.querySelector("#test");
+//   let drinkName = document.createElement("h3");
+//   drinkName.innerHTML = drink.drinks[0].strDrink;
+//   randomSearch.append(drinkName);
+//   let drinkImage = document.createElement("img");
+//   drinkImage.src = drink.drinks[0].strDrinkThumb;
+//   randomSearch.append(drinkImage);
+// // }
